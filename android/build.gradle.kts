ff3b42c5b62d6d07bc8417056f8b50bc8c2d10bc
wikkyardiansyah <wikkyardiansyah@github.com>
2026-01-06 20:42:46 +0700

    Fix Gradle repository conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,16 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+// android/build.gradle.kts
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+allprojects {
+    // KOSONG, JANGAN TAMBAH REPOSITORIES
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }

679b549c02d69379c66b41632d8f2bcd375ff457
wikkyardiansyah <wikkyardiansyah@github.com>
2026-01-06 21:36:23 +0700

    Fix: add repositories to buildscript

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,13 @@
-allprojects {}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
